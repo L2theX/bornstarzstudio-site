59319194599182a0f3fd86ecb97f7752658d1dd5
L2theX <l2thex@gmail.com>
2026-05-25 16:56:55 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
         }}
       >
         <img
-  src="/PUT-ARIA-FILENAME-HERE"
+  src="/public/71a024787425118b371251945e6fda3b832da638ca497a00010e0f0cf92a472a.webp"
   alt="Aria Esme Logo"
   style={{
     width: '260px',
