18bc000605c76ec734806b473680b7aa0c3689bc
L2theX <l2thex@gmail.com>
2026-05-25 19:54:30 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
   style={{
     width: '520px',
     borderRadius: '20px',
-    marginTop: '-320px',
+    marginTop: '-360px',
     marginBottom: '20px',
   }}
 />
